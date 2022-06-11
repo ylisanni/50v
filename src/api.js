5e754843758cf14f5ab7f58b150e4c46fb4077a8
mjturt <maks.turtiainen@gmail.com>
2022-06-11 21:05:20 +0300

    Default apiurl

--- src/api.js
+++ src/api.js
@@ -1,9 +1,10 @@
-import axios from 'axios';
+import axios from "axios";
 
-const apiUrl = process.env.REACT_APP_API_URL;
+const apiUrl =
+  process.env.REACT_APP_API_URL || "http://localhost:3000/api/participants/";
 export default axios.create({
-    baseURL: apiUrl,
-    timeout: 120,
-    responseType: 'json',
-    headers: {"Content-Type": "application/json"},
+  baseURL: apiUrl,
+  timeout: 120,
+  responseType: "json",
+  headers: { "Content-Type": "application/json" },
 });
