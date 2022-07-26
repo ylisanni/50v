9b541567f0cecb53b8c7c80457fcf6e774bc3f1b
ylisanni <ylisanni@gmail.com>
2022-07-26 18:41:57 +0300

    Update Ilmoformi.js
    Vaihdettu sana kasvis vegaaniksi, myös englanniksi

--- src/Components/Ilmoformi.js
+++ src/Components/Ilmoformi.js
@@ -272,7 +272,7 @@ export default function Ilmoformi(props) {
                         value="VG"
                       />
                       <span className="radiothing">
-                        {props.lang === "en" ? "Vegetarian" : "Kasvis"}
+                        {props.lang === "en" ? "Vegan" : "Vagaani"}
                       </span>
                     </label>
                   </div>
