48c1f2de8633510860bf4d89b03967709142b124
mjturt <maks.turtiainen@gmail.com>
2022-07-28 23:45:59 +0300

    adjust timeout

--- src/api.js
+++ src/api.js
@@ -4,7 +4,7 @@ const apiUrl =
   process.env.REACT_APP_API_URL || "http://localhost:3000/api/participants/";
 export default axios.create({
   baseURL: apiUrl,
-  timeout: 1200,
+  timeout: 5000,
   responseType: "json",
   headers: { "Content-Type": "application/json" },
 });
